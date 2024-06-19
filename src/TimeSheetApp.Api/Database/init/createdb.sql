deb12f5cbc8cab2138c40e857a17f466fbc421b5
Andrei Ciuvilin <aciuvilin@morneaushepell.com>
2024-06-19 16:32:42 -0400

    adjusted db initialization script

--- src/TimeSheetApp.Api/Database/init/createdb.sql
+++ src/TimeSheetApp.Api/Database/init/createdb.sql
@@ -121,15 +121,15 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'844e71eb-684b-466e-af4d-1a7b86d4f5b6', N'Dick', N'Dick', N'Richard', N'rd@email.com', CAST(N'1998-01-01T00:00:00.0000000' AS DateTime2), CAST(N'1968-01-01T00:00:00.0000000' AS DateTime2))
+INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'844e71eb-684b-466e-af4d-1a7b86d4f5b6', N'DRichard', N'Dick', N'Richard', N'rd@email.com', CAST(N'1998-01-01T00:00:00.0000000' AS DateTime2), CAST(N'1968-01-01T00:00:00.0000000' AS DateTime2))
 GO
-INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'59efd189-8306-41d0-a3fc-381d6fb0adfe', N'Dick', N'Rocky', N'Balboa', N'rb@email.com', CAST(N'1997-01-01T00:00:00.0000000' AS DateTime2), CAST(N'2016-03-01T00:00:00.0000000' AS DateTime2))
+INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'59efd189-8306-41d0-a3fc-381d6fb0adfe', N'RBalboa', N'Rocky', N'Balboa', N'rb@email.com', CAST(N'1997-01-01T00:00:00.0000000' AS DateTime2), CAST(N'2016-03-01T00:00:00.0000000' AS DateTime2))
 GO
-INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'5293847f-24b7-47f9-8ca0-596dbe1777d6', N'Dick', N'Johnny', N'Cash', N'jc@email.com', CAST(N'1968-01-01T00:00:00.0000000' AS DateTime2), CAST(N'1989-01-01T00:00:00.0000000' AS DateTime2))
+INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'5293847f-24b7-47f9-8ca0-596dbe1777d6', N'JCash', N'Johnny', N'Cash', N'jc@email.com', CAST(N'1968-01-01T00:00:00.0000000' AS DateTime2), CAST(N'1989-01-01T00:00:00.0000000' AS DateTime2))
 GO
-INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'682131e0-bfcd-4d7f-99a8-9408a64ce825', N'Dick', N'Georges', N'Washington', N'gw@email.com', CAST(N'1796-01-01T00:00:00.0000000' AS DateTime2), CAST(N'1796-01-01T00:00:00.0000000' AS DateTime2))
+INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'682131e0-bfcd-4d7f-99a8-9408a64ce825', N'GWashington', N'Georges', N'Washington', N'gw@email.com', CAST(N'1796-01-01T00:00:00.0000000' AS DateTime2), CAST(N'1796-01-01T00:00:00.0000000' AS DateTime2))
 GO
-INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'94c74f99-427a-43d1-a822-cf0fe25d6834', N'Dick', N'Arnold', N'Schwarzenegger', N'as@email.com', CAST(N'2016-01-01T00:00:00.0000000' AS DateTime2), CAST(N'2016-01-01T00:00:00.0000000' AS DateTime2))
+INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'94c74f99-427a-43d1-a822-cf0fe25d6834', N'ASchwarzenegger', N'Arnold', N'Schwarzenegger', N'as@email.com', CAST(N'2016-01-01T00:00:00.0000000' AS DateTime2), CAST(N'2016-01-01T00:00:00.0000000' AS DateTime2))
 GO
-INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'10caa521-18ef-425a-ba3a-d133651f6c3b', N'Dick', N'Donald', N'Trump', N'dt@email.com', CAST(N'2016-02-01T00:00:00.0000000' AS DateTime2), CAST(N'2016-02-01T00:00:00.0000000' AS DateTime2))
+INSERT [dbo].[Users] ([id], [username], [first_name], [last_name], [email], [date_of_birth], [date_created]) VALUES (N'10caa521-18ef-425a-ba3a-d133651f6c3b', N'DTrump', N'Donald', N'Trump', N'dt@email.com', CAST(N'2016-02-01T00:00:00.0000000' AS DateTime2), CAST(N'2016-02-01T00:00:00.0000000' AS DateTime2))
 GO
